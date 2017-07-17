6c3783cbe3d137123aad3c87a39babd254e5a2ed
Emmanuel Hmar <eman.hmar@gmail.com>
2017-07-17 15:56:48 -0400

    Main to default

--- Main.java
+++ Main.java
@@ -2,7 +2,21 @@ package wormgame;
 
 public class Main {
     public static void main(String[] args) throws Throwable {
-
+        WormGame game = new WormGame(20, 20);
+ 
+        UserInterface ui = new UserInterface(game, 20);
+        SwingUtilities.invokeLater(ui);
+ 
+        while (ui.getUpdatable() == null) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException ex) {
+                System.out.println("The drawing board hasn't been created yet.");
+            }
+        }
+ 
+        game.setUpdatable(ui.getUpdatable());
+        game.start();
     }
 
     }
